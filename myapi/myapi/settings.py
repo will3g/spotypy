467cbf868e5479b6297535e3d3965d33ad388f52
will3g <willianpaulo_09@hotmail.com>
2020-06-26 18:37:22 -0300

    add-variables-in-settings-to-django-filter

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'rest_framework'
+    'rest_framework',
+    'django_filters',
+    'oauth2_provider'
 ]
 
 MIDDLEWARE = [
@@ -80,6 +82,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    )
 }
 
 # Database
