82b35d82a3874f02554f2a15050e482070a414b0
will3g <willianpaulo_09@hotmail.com>
2020-06-26 18:25:17 -0300

    add-variable-rest-framework

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -71,6 +71,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myapi.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.TokenAuthentication',
+        # for browsable api view usage
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
